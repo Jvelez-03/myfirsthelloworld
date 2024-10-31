d448fa35ebfdca1695531e1b9eb3114104e17a6e
Jvelez0312 <johanvelez312068@correo.itm.edu.co>
2024-10-30 22:00:03 -0500

    solicitamos las primeras variables

---
+++ ConsoleApp1/ConsoleApp1/Estudiante.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Estudiante
+    {
+        public int Creditos { get; set; }
+        public int Estrato { get; set; }
+        public decimal ValorCredito { get; set; }
+
+        public Estudiante(int creditos, int estrato, decimal valorCredito)
+        {
+            Creditos = creditos;
+            Estrato = estrato;
+            ValorCredito = valorCredito;
+        }
+
+        public decimal CalcularMatricula()
+        {
+            decimal costo = Creditos > 20 ? (20 * ValorCredito) + (Creditos - 20) * ValorCredito * 2 : Creditos * ValorCredito;
+            decimal descuento = Estrato == 1 ? 0.2m : Estrato == 2 ? 0.5m : Estrato == 3 ? 0.7m : 1;
+            return costo * descuento;
+        }
+
+        public decimal CalcularSubsidio() => Estrato == 1 ? 200000 : Estrato == 2 ? 100000 : 0;
+    }
+
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,6 +2,10 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Ingrese el número de créditos:");
+        int creditos = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el estrato (1, 2 o 3):");
+        int estrato = int.Parse(Console.ReadLine());
     }
 }
